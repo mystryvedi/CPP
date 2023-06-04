1007f851efb349be761d4c6210808b0d3c543648
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2023-06-04 10:47:40 +0530

    Update Basic.cpp

--- Basic.cpp
+++ Basic.cpp
@@ -5,3 +5,10 @@ int main(){
     int num = 5;
     string s = "hello";
     int arr[5] = {1, 2, 3, 4, 10};
+
+    cout << num << '\n';
+    cout << s << '\n';
+    cout << arr[4] << '\n';
+
+    return 0;
+}
