e45e832df3ca6a1ed8a1d8bb655698eb7aa60ad8
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2023-12-07 18:19:31 +0530

    Update 2D Point Class.cpp

--- 2D Point Class.cpp
+++ 2D Point Class.cpp
@@ -6,3 +6,18 @@ class Point {
 private:
     double x;
     double y;
+public:
+    // Constructor
+    Point(double xCoord, double yCoord) : x(xCoord), y(yCoord) {}
+
+    // Getter methods
+    double getX() const { return x; }
+    double getY() const { return y; }
+
+    // Method to calculate the distance between two points
+    double distanceTo(const Point& otherPoint) const {
+        double deltaX = x - otherPoint.getX();
+        double deltaY = y - otherPoint.getY();
+        return std::sqrt(deltaX * deltaX + deltaY * deltaY);
+    }
+};
