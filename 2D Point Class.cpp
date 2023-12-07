9dfa794d4227621edeec766de66f30d39d44dc17
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2023-12-07 18:19:51 +0530

    Update 2D Point Class.cpp

--- 2D Point Class.cpp
+++ 2D Point Class.cpp
@@ -21,3 +21,18 @@ public:
         return std::sqrt(deltaX * deltaX + deltaY * deltaY);
     }
 };
+int main() {
+    // Create two Point objects
+    Point point1(1.0, 2.0);
+    Point point2(4.0, 6.0);
+
+    // Display the coordinates of the points
+    std::cout << "Point 1: (" << point1.getX() << ", " << point1.getY() << ")\n";
+    std::cout << "Point 2: (" << point2.getX() << ", " << point2.getY() << ")\n";
+
+    // Calculate and display the distance between the points
+    double distance = point1.distanceTo(point2);
+    std::cout << "Distance between Point 1 and Point 2: " << distance << std::endl;
+
+    return 0;
+}
