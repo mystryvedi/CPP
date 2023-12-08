a472f50134229b033a9132b39b522cb68ce224d1
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2023-12-08 18:41:16 +0530

    Update dynamic array average.cpp

--- dynamic array average.cpp
+++ dynamic array average.cpp
@@ -6,7 +6,9 @@ double findAverage(const int* arr, int size) {
         std::cerr << "Error: Empty array." << std::endl;
         return 0.0;
     }
-double sum = 0.0;
-    for (int i = 0; i < size; ++i) {
-        sum += arr[i];
-    }
+    double sum = 0.0;
+        for (int i = 0; i < size; ++i) {
+            sum += arr[i];
+        }
+return sum / static_cast<double>(size);
+}
