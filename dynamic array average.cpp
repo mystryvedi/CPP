0ff6d674156f31827dadb094e1aa3ef57dbfb603
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2023-12-08 18:40:29 +0530

    Update dynamic array average.cpp

--- dynamic array average.cpp
+++ dynamic array average.cpp
@@ -1,1 +1,8 @@
+#include <iostream>
 
+// Function to find the average of an array
+double findAverage(const int* arr, int size) {
+    if (size <= 0) {
+        std::cerr << "Error: Empty array." << std::endl;
+        return 0.0;
+    }
