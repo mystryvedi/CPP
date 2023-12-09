8cce0b3c2b73ca0badd6d304a5af3abb7c27d3e3
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2023-12-09 18:27:29 +0530

    Update Exception Handling Calculator.cpp

--- Exception Handling Calculator.cpp
+++ Exception Handling Calculator.cpp
@@ -8,3 +8,13 @@ double divide(double numerator, double denominator) {
     }
     return numerator / denominator;
 }
+
+int main() {
+    double num1, num2;
+
+    // Input: Get two numbers from the user
+    std::cout << "Enter the first number: ";
+    std::cin >> num1;
+
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
