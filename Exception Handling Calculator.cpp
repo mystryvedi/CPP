3e52ef15ad01c978ce5cfe0eee1c556200922c1f
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2023-12-09 18:27:09 +0530

    Update Exception Handling Calculator.cpp

--- Exception Handling Calculator.cpp
+++ Exception Handling Calculator.cpp
@@ -1,2 +1,10 @@
 #include <iostream>
 #include <stdexcept> // for std::runtime_error
+
+// Function to perform division with exception handling
+double divide(double numerator, double denominator) {
+    if (denominator == 0.0) {
+        throw std::runtime_error("Error: Division by zero.");
+    }
+    return numerator / denominator;
+}
