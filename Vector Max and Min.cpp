936d2beb0e11c729cb11cfcfc2d0e38d088a86b4
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2023-12-11 19:25:36 +0530

    Update Vector Max and Min.cpp

--- Vector Max and Min.cpp
+++ Vector Max and Min.cpp
@@ -1,3 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
+// Function to find the maximum and minimum values in a vector
+void findMaxAndMin(const std::vector<int>& vec) {
+    if (vec.empty()) {
+        std::cerr << "Error: Empty vector." << std::endl;
+        return;
+    }
