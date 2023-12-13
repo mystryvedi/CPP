084e42568243c47d246652fa7a8225884247a0b5
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2023-12-13 22:22:00 +0530

    Update Word Reversal.cpp

--- Word Reversal.cpp
+++ Word Reversal.cpp
@@ -7,3 +7,8 @@ std::string reverseWords(const std::string& input) {
     std::istringstream iss(input);
     std::ostringstream oss;
     std::string word;
+
+    while (iss >> word) {
+        // Add the reversed word to the output stream
+        oss << std::string(word.rbegin(), word.rend()) << " ";
+    }
