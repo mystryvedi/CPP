683f6e7643d11ff6c34bbf950d988269c4afa8b6
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2023-12-13 22:21:28 +0530

    Update Word Reversal.cpp

--- Word Reversal.cpp
+++ Word Reversal.cpp
@@ -1,3 +1,7 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+
+// Function to reverse the order of words in a string
+std::string reverseWords(const std::string& input) {
+    std::istringstream iss(input);
