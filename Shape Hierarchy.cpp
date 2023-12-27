9d2a69c6a0eaed819bbc10352969fc31acb8eafa
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2023-12-27 17:29:40 +0530

    Update Shape Hierarchy.cpp

--- Shape Hierarchy.cpp
+++ Shape Hierarchy.cpp
@@ -7,3 +7,9 @@ public:
     virtual double perimeter() const = 0; // Pure virtual function for perimeter calculation
     virtual ~Shape() {} // Virtual destructor for polymorphic behavior
 };
+
+// Derived class representing a rectangle
+class Rectangle : public Shape {
+private:
+    double length;
+    double width;
