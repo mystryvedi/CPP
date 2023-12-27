9b14bd02c2a178f99397ecf90ba68fb879189a3e
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2023-12-27 17:29:57 +0530

    Update Shape Hierarchy.cpp

--- Shape Hierarchy.cpp
+++ Shape Hierarchy.cpp
@@ -13,3 +13,15 @@ class Rectangle : public Shape {
 private:
     double length;
     double width;
+
+public:
+    Rectangle(double l, double w) : length(l), width(w) {}
+
+    double area() const override {
+        return length * width;
+    }
+
+    double perimeter() const override {
+        return 2 * (length + width);
+    }
+};
