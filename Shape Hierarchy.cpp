7c1e58fff6f42f2d840aed21235508b3680783aa
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2023-12-27 17:29:25 +0530

    Update Shape Hierarchy.cpp

--- Shape Hierarchy.cpp
+++ Shape Hierarchy.cpp
@@ -1,1 +1,9 @@
+#include <iostream>
 
+// Base class representing a shape
+class Shape {
+public:
+    virtual double area() const = 0; // Pure virtual function for area calculation
+    virtual double perimeter() const = 0; // Pure virtual function for perimeter calculation
+    virtual ~Shape() {} // Virtual destructor for polymorphic behavior
+};
